853b4e1f7a80b0681cc425ac2521cab4e395c242
ghosterenko <arsenijkonkov243@gmail.com>
2026-03-04 09:52:06 +0500

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -5,6 +5,26 @@
 int main()
 {
     setlocale(LC_ALL, "ru");
+
+    std::cout << "Выбирете процесс: " << std::endl;
+    std::cout << "1. Фоновый: " << std::endl;
+    std::cout << "2. Нормальный: " << std::endl;
+    std::cout << "3. Высокий: " << std::endl;
+    char n;
+    switch (n) {
+    case 1:
+        SetPriorityClass(GetCurrentProcess(), IDLE_PRIORITY_CLASS);
+        break;
+    case 2:
+        SetPriorityClass(GetCurrentProcess(), NORMAL_PRIORITY_CLASS);
+        break;
+    case 3:
+        SetPriorityClass(GetCurrentProcess(), HIGH_PRIORITY_CLASS);
+        break;
+    default:
+        break;
+    }
+
     HANDLE OpenMutexPrinter = OpenMutexA(MUTANT_ALL_ACCESS, FALSE, "PrinterMutex");
     if (OpenMutexPrinter == NULL) {
         std::cout << "Принтер не найден" << std::endl;
@@ -13,18 +33,13 @@ int main()
     std::cin.get();
     DWORD wait = WaitForSingleObject(OpenMutexPrinter, INFINITE);
     if (wait == WAIT_OBJECT_0) {
-        WaitForSingleObject(OpenMutexPrinter, INFINITE);
         std::cout << "Задание отправлено на печать" << std::endl;
         ReleaseMutex(OpenMutexPrinter);
     }
     WaitForSingleObject(OpenMutexPrinter, INFINITE);
-    ReleaseMutex(OpenMutexPrinter);
     std::cout << "Печать завершена" << std::endl;
-
-    WaitForSingleObject(OpenMutexPrinter, INFINITE);
     ReleaseMutex(OpenMutexPrinter);
 
     CloseHandle(OpenMutexPrinter);
     return 0;
 }
-

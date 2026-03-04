5e75439987447e6cb5eed6eea978343ebbff0951
ghosterenko <arsenijkonkov243@gmail.com>
2026-03-04 08:54:26 +0500

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -5,17 +5,25 @@
 int main()
 {
     setlocale(LC_ALL, "ru");
-    std::cout << "Клиент" << std::endl;
-    HANDLE OpenMutexPrinter = OpenMutex(NULL, FALSE, L"PrinterMutex");
+    HANDLE OpenMutexPrinter = OpenMutexA(MUTANT_ALL_ACCESS, FALSE, "PrinterMutex");
     if (OpenMutexPrinter == NULL) {
         std::cout << "Принтер не найден" << std::endl;
+        return 1;
     }
-    else {
-        WaitForSingleObject(OpenMutexPrinter, INFINITE);
-        std::cout << "Задание отправлено на печать" << std::endl;
+    WaitForSingleObject(OpenMutexPrinter, INFINITE);
+    std::cout << "Клиент подключен" << std::endl;
+    std::cin.get();
+    std::cout << "Ожидание подключения к принтеру" << std::endl;
+    ReleaseMutex(OpenMutexPrinter);
+    std::cout << "Задание отправлено на печать" << std::endl;
+    DWORD wait = WaitForSingleObject(OpenMutexPrinter, INFINITE);
+    if (wait == WAIT_OBJECT_0) {
         ReleaseMutex(OpenMutexPrinter);
         std::cout << "Печать завершена" << std::endl;
     }
-    _getch();
-
+    WaitForSingleObject(OpenMutexPrinter, INFINITE);
+    ReleaseMutex(OpenMutexPrinter);
+    CloseHandle(OpenMutexPrinter);
+    return 0;
 }
+
